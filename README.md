3faf9bf47a4f30b031f8c7f62c86c527c2a359a5
Khang Huynh <23klhuynh@gmail.com>
2024-12-30 23:48:03 -0500

    Implementing more features and fixing bugs.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Premier League soccer match prediction
+
+## An application that utilizes a machine learning model to analyze data from several past seasons Premier League to predict upcoming matches!
+
+This project utilizes a machine learning model to analyze data from several past Premier League seasons and predict upcoming match outcomes. As a passionate soccer fan, especially of the Premier League, I built this application to help predict which teams will win, lose, or draw. The Premier League’s intensity and high level of play make it a thrilling league to follow, and applying machine learning to this project makes it even more exciting.
+This is my first project utilizing Jupyter Notebook, and it has been a great opportunity to improve my knowledge in AI and machine learning.
+
+## Features
+
+* Utilized team statistics data from several past seasons, including metrics like pass accuracy, effective tackles, saves, and more.
+
+* Trained and tested a machine learning model using historical data to predict upcoming matches.
+
+* Brainstorm the advantages and disadvantages for a team, such as home-field advantage, climate, possession, and more to refine predictions.
+
+## Result
+
+* The model performs accurately when predicting outcomes for the top half of the table teams such as Manchester City, Liverpool, Arsenal, Chelsea, Manchester United, Tottenham Hotspur, Newcastle United, Aston Villa, Brighton & Hove Albion, and West Ham United. These teams have a long history and competitiveness at the highest level, resulting in the model making better predictions.
+
+* Predictions are less reliable for teams in the bottom half of the table due to the unpredictability of teams fighting against relegation. These teams often show erratic performance when fighting for better positions.
+
+## How to use the application
--- experiment/main.py
+++ experiment/main.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score
 
-matches = pd.read_csv("assets/2023-2024.csv")
+matches = pd.read_csv("../assets/2023-2024.csv")
 matches["Date Time (US Eastern)"] = pd.to_datetime(matches["Date Time (US Eastern)"])
 
 #---advantages and disadvantages to consider when teams play against each other---
@@ -41,12 +41,8 @@ def rolling_averages(group, cols, new_cols):
     return group.dropna(subset=new_cols)
 
 #---Add rolling averages---
-columns = [
-    "totalShots", "shotsOnTarget", 
-    "possessionPct", "wonCorners", 
-    "accuratePasses", "totalPasses", 
-    "effectiveTackles", "totalTackles",
-    ]
+columns = ["totalShots", "totalPasses"]
+
 new_columns = [f"{c}_rolling" for c in columns]
 
 matches_rolling = matches.groupby("Team").apply(lambda x: rolling_averages(x, columns, new_columns))
@@ -72,7 +68,12 @@ def make_predictions(data, predictors, target_col='target'):
     
     return combine, accuracy, precision
 #
-predictors = ["homeAway_code", "opponent_code", "hour", "day_code"] + new_columns
+predictors = [   
+    "homeAway_code", "opponent_code", "day_code", 
+    "shotsOnTarget", "accuratePasses", "accurateCrosses", 
+    "accurateLongBalls", "blockedShots", "effectiveTackles", 
+    "effectiveClearance","foulsCommitted", "saves"
+    ] + new_columns
 
 #---predictions---
 combined, accuracy, precision = make_predictions(matches_rolling, predictors)
@@ -86,6 +87,36 @@ combined = combined.merge(
 """ print(combined.head())
 print(precision)
  """
+
+def predict_next_match(home_team, away_team, date_time):
+
+    if home_team not in matches["Team"].unique() or away_team not in matches["Team"].unique():
+        print("error")
+        return
+    
+    #hour = pd.to_datetime(date_time).hour
+    day_code = pd.to_datetime(date_time).dayofweek
+
+    homeAway_code = 0 if home_team == home_team else 1
+    opponent_code = matches["Team"].astype("category").cat.categories.get_loc(away_team)
+
+    rolling_values = [0] * len(new_columns)  
+
+    match_data = pd.DataFrame([{
+        "homeAway_code": homeAway_code,
+        "opponent_code": opponent_code,
+        #"hour": hour,
+        "day_code": day_code,
+        **{col: val for col, val in zip(new_columns, rolling_values)}
+        
+    }])
+    match_data = match_data[predictors]
+    
+    prediction = rf.predict(match_data)
+    result = "Win" if prediction[0] == 1 else "Loss" if prediction[0] == 0 else "Draw"
+
+    print(f"Prediction for {home_team} vs {away_team}: {result}")
+
 
 
 
